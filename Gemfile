46b4dc42261d041ca5a8ba18fa941d8e73114784
Bruno Miranda <bruno@bopia.com>
2011-02-28 11:25:11 -0500

    Initial Project.

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.5'
+gem 'heroku'
+gem 'haml'
+gem 'sqlite3'
+gem 'mysql'
+
+group :test, :development do
+  gem "rspec-rails", "~> 2.4"
+  gem 'sqlite3-ruby', :require => 'sqlite3'  
+  gem 'ruby-debug19', :require => 'ruby-debug'  
+end
+
+# Bundle edge Rails instead:
+# gem 'rails', :git => 'git://github.com/rails/rails.git'
+# Bundle the extra gems:
+# gem 'bj'
+# gem 'nokogiri'
+# gem 'aws-s3', :require => 'aws/s3'
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vintenews::Application.config.secret_token = 'b5c0e4897804e631942fcb72e5bdc1bc756cb8b47a9aeff3f6f017a47c5fb4b9f98da44547e598cd0c2b084a0338020f516e4841527f917eaf5fb76ad55c3cac'
