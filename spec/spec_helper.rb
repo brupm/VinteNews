2b9ef1aecf9dc94620361e6650643a15b71f5d81
Davis Zanetti Cabral <daviscabral@gmail.com>
2011-03-05 17:29:37 -0300

    Spec fixes and initial work with capybara

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,44 +2,21 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-require 'capybara/rails'
-require 'capybara/dsl'
 require 'authlogic/test_case'
-require 'integration_spec_helper'
+require 'capybara/rspec'
+require 'capybara/rails'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+Dir[Rails.root.join("spec/spec_helpers/**/*.rb")].each {|f| require f}
+
+include IntegrationSpecHelper
 
 RSpec.configure do |config|
-  # == Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
   config.mock_with :rspec
-
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
   config.use_transactional_fixtures = true
-
-  config.include Capybara, :type => :integration
-  config.include IntegrationSpecHelper #, :type => :integration
-
-  Capybara.default_wait_time = 5
-  Capybara.ignore_hidden_elements = false
-
-  config.after :each do
-    Capybara.use_default_driver
-    Capybara.default_host = "www.example.com"
-    Capybara.reset_sessions!
-  end
 end
 
 class ActionController::TestCase
