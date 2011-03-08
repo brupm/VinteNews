59d73d5a0e0672161c03aabd5ed30e8adf7d865c
Bruno Miranda <bruno@bopia.com>
2011-03-08 12:20:57 -0500

    Small fixes.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,7 @@ class ApplicationController < ActionController::Base
   def require_user
     unless current_user
       store_location
-      redirect_to new_user_session_url, :notice => "You must be logged in to access this page"
+      redirect_to new_user_session_url, :notice => "Voce deve estar logado para acessar essa pagina."
       return false
     end
   end
@@ -28,7 +28,7 @@ class ApplicationController < ActionController::Base
   def require_no_user
     if current_user
       store_location
-      redirect_to root_url, :notice => "You must be logged out to access this page"
+      redirect_to root_url, :notice => "Voce deve estar logado para acessar essa pagina."
       return false
     end
   end
