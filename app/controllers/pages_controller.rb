2e6998274eca1b08d56db72242cd8b6077443830
Bruno Miranda <bruno@bopia.com>
2011-04-28 16:56:03 -0400

    Fixing dumb bug.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,6 @@ class PagesController < ApplicationController
   end
   
   def lists
-    @popular_users = User.all(:limit => 20).reject{ |u| u.karma < 2}.sort_by { |u| u.karma }.reverse
+    @popular_users = User.all.reject{ |u| u.karma < 2}.sort_by { |u| u.karma }.reverse
   end
 end
